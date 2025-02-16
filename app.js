5e209c6c6d2a34ed97dd28674cc6b83f03d69751
inshah20 <inshahjamal26@gmail.com>
2025-02-16 11:10:55 +0500

    created app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+// chapter 17-20:
+// Loops and arrays:
+
+// Qno:1 Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+// ans;
+// let array = [];
+
+// Qno:2 Declare and initialize a multidimensional array
+// representing the following matrix:
+// ans;
+// let array = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]]
+
+// for(i=0; i<=3; i++){
+//     for(j=0; j<=4; j++){
+//         document.write(array[i][j]);
+//     }
+//     document.write("<br />");
+// }
+
+// Qno:3 Write a program to print numeric counting from 1 to 10.
+// ans;
+// for(i=1; i<=10; i++){
+//     document.write(i + "<br />");
+// }
